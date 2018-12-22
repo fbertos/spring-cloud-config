71763d7ff99ea3f50f89ab70eb6c5947c01df653
Fernando Bertos Matilla <fbertos@gmail.com>
2018-12-22 12:21:09 +0100

    Version 1.0

---
+++ src/main/java/com/autentia/spring/cloud/config/server/ApplicationConfigServer.java
@@ -0,0 +1,14 @@
+package com.autentia.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableAutoConfiguration
+@EnableConfigServer
+public class ApplicationConfigServer {
+
+ public static void main(String[] args) {
+	 SpringApplication.run(ApplicationConfigServer.class, args);
+ }
+}
